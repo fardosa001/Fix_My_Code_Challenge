228e477b1eb2f442b98ee387c6dadb43c0f1ae29
fardosa001 <fardosaadow@gmail.com>
2023-10-29 11:51:54 -0700

    fix

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -37,7 +37,9 @@ class User():
         - `None` if `pwd` is not a string
         - Hash `pwd` in MD5 before assign to `__password`
         """
-        if pwd is not None and type(pwd) is str:
+        if pwd is None or type(pwd) is not str:
+            self.__password = None
+        else:
             self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
