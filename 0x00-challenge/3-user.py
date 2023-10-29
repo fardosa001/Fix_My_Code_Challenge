324db840e334c850e7e2e90aec155a1891bb48c1
fardosa001 <fardosaadow@gmail.com>
2023-10-29 11:36:11 -0700

    fix

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -52,7 +52,7 @@ class User():
             return False
         if self.__password is None:
             return False
-        
+
         return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
 
 
